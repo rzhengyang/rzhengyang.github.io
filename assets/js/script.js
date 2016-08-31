ccb73bc97ed64e7f14f2da51e8f0bc17b3439d3d
billy.ruzheng <billy.ruzheng@gmail.com>
2016-08-31 13:53:08 +0800

    change tags

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,12 @@ if ($(window).width() <= 1280) {
 }
 
 // Variables
-    tag1       = $('.pl__all'),
-    tag2       = $('.place_your_tag_name_here'),
-    tag3       = $('.place_your_tag_name_here'),
-    tag4       = $('.place_your_tag_name_here'),
-    tag5       = $('.place_your_tag_name_here'),
-    tag6       = $('.place_your_tag_name_here');
+    tag1       = $('.全部'),
+    tag2       = $('.技术'),
+    tag3       = $('.旅行'),
+    tag4       = $('.读书'),
+    tag5       = $('.精分'),
+    tag6       = $('.关于我');
 var sidebar    = $('#sidebar'),
     container  = $('#post'),
     content    = $('#pjax'),
